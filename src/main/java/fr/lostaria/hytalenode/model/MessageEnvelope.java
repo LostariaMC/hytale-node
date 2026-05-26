71a2ed404cd3004d736509461a48a16daff3294c
Erpriex <erpriex@gmail.com>
2026-05-26 23:54:59 +0200

    🚑️ Add consumer to message protocol

--- src/main/java/fr/lostaria/hytalenode/model/MessageEnvelope.java
+++ src/main/java/fr/lostaria/hytalenode/model/MessageEnvelope.java
@@ -3,6 +3,7 @@ package fr.lostaria.hytalenode.model;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public record MessageEnvelope(
+        String consumer,
         String type,
         String producer,
         JsonNode payload
